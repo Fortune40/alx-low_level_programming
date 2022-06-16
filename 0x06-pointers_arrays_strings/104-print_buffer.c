47c186f837f211fdf3e14327a3354cbe18d20102
Daniel <CJNwagbara@hotmail.com>
2022-06-16 14:51:38 +0000

    lasttsk

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,7 +16,7 @@ else
 for (x = 0; x < size; x += 10)
 {
 printf("%.8x:", x);
-for (y = x; y < x + 10; k++)
+for (y = x; y < x + 10; y++)
 {
 if (y % 2 == 0)
 printf(" ");
@@ -30,10 +30,10 @@ for (z = x; z < x + 10; z++)
 {
 if (z >= size)
 break;
-if (*(b + l) < 32 || *(b + l) > 126)
+if (*(b + z) < 32 || *(b + z) > 126)
 printf("%c", '.');
 else
-printf("%c", *(b + l));
+printf("%c", *(b + z));
 }
 printf("\n");
 }
