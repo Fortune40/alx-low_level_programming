c6bc28a19d6e1446d64b150641c526601b88f190
Daniel <CJNwagbara@hotmail.com>
2022-07-05 23:47:12 +0000

    tsk fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,11 +8,11 @@
 
 unsigned int _strlen(char *s)
 {
-  unsigned int len = 0;
+unsigned int len = 0;
 
-  while (*(s + len))
-    len++;
-  return (len);
+while (*(s + len))
+len++;
+return (len);
 }
 
 /**
@@ -24,19 +24,19 @@ unsigned int _strlen(char *s)
 
 char *_strcpy(char *s, int len)
 {
-  char *new_s;
-  int i = 0;
+char *new_s;
+int i = 0;
 
-  new_s = malloc(sizeof(char) * (len + 1));
-  if (!new_s)
-    return (NULL);
-  while (s[i])
-    {
-      new_s[i] = s[i];
-      i++;
-    }
-  new_s[i] = s[i];
-  return (new_s);
+new_s = malloc(sizeof(char) * (len + 1));
+if (!new_s)
+return (NULL);
+while (s[i])
+{
+new_s[i] = s[i];
+i++;
+}
+new_s[i] = s[i];
+return (new_s);
 }
 
 /**
@@ -50,27 +50,27 @@ char *_strcpy(char *s, int len)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  dog_t *d;
-  char *cpy_name, *cpy_owner;
+dog_t *d;
+char *cpy_name, *cpy_owner;
 
-  d = malloc(sizeof(dog_t));
-  if (!d)
-    return (NULL);
-  /* Copy strings */
-  cpy_name = _strcpy(name, _strlen(name));
-  cpy_owner = _strcpy(owner, _strlen(owner));
-  /* If fail someone free and return NULL */
-  if (!cpy_name || !cpy_owner)
-    {free(d);
-      if (cpy_name)
-	free(cpy_name);
-      if (cpy_owner)
-	free(cpy_owner);
-      return (NULL);
-    }
-  /* Otherwise assing name and owner */
-  d->name = cpy_name;
-  d->age = age;
-  d->owner = cpy_owner;
-  return (d);
+d = malloc(sizeof(dog_t));
+if (!d)
+return (NULL);
+/* Copy strings */
+cpy_name = _strcpy(name, _strlen(name));
+cpy_owner = _strcpy(owner, _strlen(owner));
+/* If fail someone free and return NULL */
+if (!cpy_name || !cpy_owner)
+{free(d);
+if (cpy_name)
+free(cpy_name);
+if (cpy_owner)
+free(cpy_owner);
+return (NULL);
+}
+/* Otherwise assing name and owner */
+d->name = cpy_name;
+d->age = age;
+d->owner = cpy_owner;
+return (d);
 }
