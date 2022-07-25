b3d025fd6453b8b46a53e5a1af3b717ca52605b3
Daniel <CJNwagbara@hotmail.com>
2022-07-25 23:05:03 +0000

    tsklst

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,28 +10,21 @@ int create_file(const char *filename, char *text_content)
 {
 int i, j, k = 0;
 
-if (!filename)
-{
+if (filename == NULL)
 return (-1);
-}
-i = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (k == -1)
+
+if (text_content != NULL)
 {
-return (-1);
-}
-if (text_content)
-{
-while (text_content[k] != '\0')
-{
+for (k = 0; text_content[k];)
 k++;
 }
+
+i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 j = write(i, text_content, k);
-}
-if (j == -1 || j != k)
-{
-close(i);
+
+if (i == -1 || j == -1)
 return (-1);
-}
+
 close(i);
 return (1);
 }
