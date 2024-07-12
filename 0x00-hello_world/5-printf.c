01ab437d351a9b2267b1c334b738a0b96abd33f9
Daniel <CJNwagbara@hotmail.com>
2024-07-12 17:26:51 +0100

    Append Files & git ignore

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints strings of character
+ * Return: zero if sucess, non-zero if fail
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
